b1e860dcf8a9a4b9db879bb87069d962d9b348b3
Andrew Hani <andrew_hany@live.com>
2022-10-26 15:48:40 +0200

    fix

--- devServer.js
+++ devServer.js
@@ -5,7 +5,7 @@
 'use strict';
 
 const express = require('express');
-const PORT = process.env.PORT || 8000;
+// const PORT = process.env.PORT || 8000;
 
 let app = express();
 
@@ -13,7 +13,7 @@ app.use('/', express.static('public') );
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
-});
+}).listen(3000);
 
 app.use((req, res) => {
   res.status(404).send('Page not found');
